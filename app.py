1664c8555e99456c71b75b58ba584bb96f814183
Annie S <amsheehan1@gmail.com>
2020-07-25 14:32:01 +0000

    app.py  add route fix

--- app.py
+++ app.py
@@ -28,10 +28,10 @@ def register():
 
 @app.route('/add_route', methods=['GET', 'POST'])
 def add_route():
-    return render_template('add_route.html', title='add a route', form=form)
+    return render_template('add_route.html', title='add a route',)
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP', '84.203.6.55'),
+    app.run(host=os.environ.get('IP', '0.0.0.0'),
             port=int(os.environ.get('PORT', '8080')),
-            debug=True)
+            debug=False)
