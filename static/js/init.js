3f94559268e95bf1e0923154938f0900ae15e021
Annie S <amsheehan1@gmail.com>
2020-08-01 12:29:17 +0000

    switch parralax to carousel

--- static/js/init.js
+++ static/js/init.js
@@ -6,3 +6,13 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+var instance = M.Carousel.init({
+    fullWidth: true
+  });
+
+  // Or with jQuery
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true
+  });
